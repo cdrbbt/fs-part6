ef44ecd9d2172f4a28f5bb34e0c26544a29c6e47
cdrbbt <egor.bykadorov@gmail.com>
2021-02-21 23:57:24 +0200

    unused variables

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
